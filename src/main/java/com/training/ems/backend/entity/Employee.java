b62371f36057163f4ebd1f1545fcd29b08c16af2
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-09-16 10:58:24 -0500

    added employee entity

---
+++ src/main/java/com/training/ems/backend/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.training.ems.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name = "first_name")
+    private String lastName;
+
+    @Column(name = "last_name")
+    private  String firstName;
+
+    @Column(name= "email_id", nullable = false, unique = true)
+    private String email;
+}
